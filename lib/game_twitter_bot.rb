c7afa8b8a7819689cc4042b064b912e52d7398e2
Kevin Yaroch <kjyaroch@gmail.com>
2017-04-07 17:15:05 -0500

    Create Database class

---
+++ lib/game_twitter_bot.rb
@@ -0,0 +1,5 @@
+$LOAD_PATH.unshift File.dirname(__FILE__)
+require 'game_twitter_bot/database'
+
+module GameTwitterBot
+end
---
+++ lib/game_twitter_bot/database.rb
@@ -0,0 +1,29 @@
+module GameTwitterBot
+  class Database
+    def initialize
+      @db = SQLite3::Database.new("games.db")
+      initialize_table
+    end
+
+    def insert(name)
+      @db.execute("INSERT INTO games (name) VALUES (?)", name)
+    end
+
+    def random_game
+      @db.execute("SELECT * FROM games ORDER BY RANDOM() LIMIT 1;")[0]
+    end
+
+    def delete(id)
+      @db.execute("DELETE FROM games WHERE id = ?;", id)
+    end
+
+    private
+
+    def initialize_table
+      @db.execute(
+        "CREATE TABLE IF NOT EXISTS games (id INTEGER PRIMARY KEY, name STRING, " \
+        "CONSTRAINT unique_name UNIQUE (name) ON CONFLICT IGNORE);"
+      )
+    end
+  end
+end
